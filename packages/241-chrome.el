b944fd5e63910547b2548dcebece58a5f89a5f9e
ChanningKuo <channingkuo@icloud.com>
2025-07-31 14:18:56 +0800

    chorme debug test

---
+++ packages/241-chrome.el
@@ -0,0 +1,18 @@
+;;; 105-dap.el --- Debug Adapter Protocol configuration -*- lexical-binding: t -*-
+;;; Commentary:
+
+;; This file for debug in chrome
+;; dap-mode: https://emacs-lsp.github.io/dap-mode/page/configuration/
+
+;;; Code:
+
+;;(use-package dap-chrome
+;;  :ensure nil
+;;  :after dap-mode
+;;  :defer t
+;;  :config
+;;  (dap-chrome-setup)
+  ;; (setq dap-auto-configure-features '(sessions locals breakpoints expressions controls tooltip))
+;;)
+
+;;; 241-chrome.el ends here
